6b7abbee1f916b115dd946366fab0aa357c1f340
Yevhenii Biriuk <yevgenbeeryuk@gmail.com>
2022-01-18 21:45:04 +0200

    func

--- 2022.01.18_dz/2022.01.18_dz/2022.01.18_dz.cpp
+++ 2022.01.18_dz/2022.01.18_dz/2022.01.18_dz.cpp
@@ -2,7 +2,22 @@
 
 #include <iostream>
 
+void func()
+{
+    cout << "Enter num: ";
+    int a;
+    cin >> a;
+    cout << endl << a << endl;
+}
+
+void func1()
+{
+    cout << "Error\n";
+    cout << ":D\n";
+}
 int main()
 {
     cout << "Hi\n";
+    func();
+    func1();
 }
